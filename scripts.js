f09752a9598393e7d899ddaf792523effd037a0d
Austin Isakson <austinjisakson@gmail.com>
2019-04-21 11:26:03 -0600

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = 'Austin Isakson';
+var speed = 120;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("headerTxt").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.onload = typeWriter;
